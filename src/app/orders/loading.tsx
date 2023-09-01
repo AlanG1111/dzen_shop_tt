61324d217e132897f83911ee6ada631b88987d7c
Alan <alan_h@evacodes.com>
2023-09-01 13:50:47 +0300

    add loading

---
+++ src/app/orders/loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoadingComponent from "@/components/LoadingComponent";
+
+const loading: React.FC = () => {
+  return <LoadingComponent />;
+};
+
+export default loading;
