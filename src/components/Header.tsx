e357234e177a117421a461790f754be82d6ecced
Alan <alan_h@evacodes.com>
2023-09-03 09:53:04 +0300

    header layout and styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 import TopMenu from "./TopMenu";
 
 const Header: React.FC = () => {
   return (
-    <header>
-      header
-      <TopMenu />
+    <header className='shadow flex-shrink-0'>
+      <div className='container d-flex h-100 align-items-center justify-content-between p-2'>
+        <Link
+          href='/'
+          className='d-flex align-items-center font-weight-bold gap-2 text-primary text-decoration-none text-uppercase text-dark'
+        >
+          <Image
+            src='/images/main_logo.png'
+            width={35}
+            height={35}
+            alt='logo'
+          />
+          Inventory
+        </Link>
+        <input
+          className='form-control me-2 w-25'
+          type='search'
+          placeholder='Search'
+        />
+        <TopMenu />
+      </div>
     </header>
   );
 };
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+"use client";
 
-const TopMenu: React.FC = () => {
-  return <div>TOP MENU</div>;
-};
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { formatDate, formatTime } from "@/helpers/dateFormatters";
 
-export default TopMenu;
+export default function DateTimeDisplay() {
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
+
+  useEffect(() => {
+    setCurrentDateTime(new Date());
+  }, []);
+
+  const formattedDate = formatDate(currentDateTime);
+  const formattedTime = formatTime(currentDateTime);
+
+  return (
+    <div className='d-flex flex-column justify-content-space-between'>
+      <div className='d-flex gap-6'>
+        <div
+          className='gap-2 d-flex align-items-center'
+          suppressHydrationWarning
+        >
+          <span>{formattedDate}</span>
+          <Image
+            src='/images/clock_icon.png'
+            width={16}
+            height={16}
+            alt='logo'
+          />
+          <span>{formattedTime}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
